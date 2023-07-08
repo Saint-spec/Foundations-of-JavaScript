e950f0784f44f7789c7959fc2d78c5e237814ef3
Saint-spec <christiancervantes02@gmail.com>
2023-07-08 14:04:16 -0400

    added basic html and css. Completed 3 basic js functions

---
+++ script.js
@@ -0,0 +1,22 @@
+/*  Sum of 2 numbers
+    Write a function that takes in 2 numbers and returns their sum. 
+
+   function addNumbers(num1, num2) {
+    return num1 + num2;
+   } 
+
+   let result = addNumbers(4, 5)
+   console.log(result); */
+   
+   
+/*  Sum of # numbers
+   function addThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+   }
+
+   let result = addThreeNumbers(2, 2, 2)
+   console.log(result) */
+
+/* Greater than 5
+   Returns true if function greater than 5*/
+

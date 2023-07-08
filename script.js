d8849636958a899053d144e021ad50924b05c6a7
Saint-spec <christiancervantes02@gmail.com>
2023-07-08 14:34:27 -0400

    added small github logo. Added 2 more basic js functions

--- script.js
+++ script.js
@@ -9,7 +9,8 @@
    console.log(result); */
    
    
-/*  Sum of # numbers
+
+/*  Sum of 3 numbers
    function addThreeNumbers(num1, num2, num3) {
     return num1 + num2 + num3;
    }
@@ -17,6 +18,47 @@
    let result = addThreeNumbers(2, 2, 2)
    console.log(result) */
 
-/* Greater than 5
-   Returns true if function greater than 5*/
 
+
+/*  Greater than 5
+   Returns true if function greater than 5
+
+   function greaterThanFive (number) {
+    return number > 5;
+   }
+
+   console.log(greaterThanFive(6));
+   console.log(greaterThanFive(3));
+   console.log(greaterThanFive(5));  */
+
+
+
+/*  Biggest number out of 2
+
+    function largerNumber(num1, num2) {
+        if (num1 > num2) {
+            return num1;
+        } else {
+           return num2
+        }
+    }
+
+    console.log(largerNumber(2345, 100000)) */
+
+/* Biggest number out of 3 
+
+function largestNum(num1, num2, num3) {
+   let largest = num1;
+
+   if (num2 > largest) {
+    largest = num2;
+   }
+
+   if (num3 > largest) {
+    largest = num3;
+   }
+   
+   return largest;
+}
+ 
+console.log(largestNum(34, 12, 12348)); */
